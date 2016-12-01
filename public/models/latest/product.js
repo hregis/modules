a7f6c4f2d047f481a4e62bd1a7ddd9019abe5013
Herve Prot <herve.prot@tomanage.fr>
2016-12-01 14:41:30 +0100

    Add replenishmentTime in model product supplier

--- public/models/latest/product.js
+++ public/models/latest/product.js
@@ -57,6 +57,7 @@ var supplierPriceSchema = new Schema({
     ref: String,
     tva_tx: Number,
     minQty: Number,
+    replenishmentTime : {type: Number, default: 0}, // delai de reappro en jr
     prices: {
         pu_ht: {type: Number, default: 0}, // For base price
         pricesQty: {type: Schema.Types.Mixed} // For quantity price reduction
