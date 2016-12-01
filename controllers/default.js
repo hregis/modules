831dc0b83bee49c55d6c826ee3ec422a56805f30
Herve Prot <herve.prot@tomanage.fr>
2016-12-01 17:49:04 +0100

    Add new function zipcode

---
+++ controllers/default.js
@@ -0,0 +1,36 @@
+exports.install = function () {
+    F.route('/', view_homepage);
+    F.route('/api/zipcode/autocomplete', zipcode);
+};
+
+function view_homepage() {
+    var self = this;
+    self.redirect('http://www.tomanage.fr');
+}
+
+
+function zipcode() {
+    var self = this;
+    var ZipCodeModel = MODEL('zipCode').Schema;
+
+    if (this.body.val === null)
+        return self.json([]);
+
+    var val = "^" + this.body.val;
+
+    var query = {"$or": [
+            {code: new RegExp(val, "i")},
+            {city: new RegExp(val, "i")}
+        ]
+    };
+
+    //var query = {$or: [{"code" : {$regex: /val.*/ }}, {"city" : { $regex: val, $options: 'i'}}]};
+
+    ZipCodeModel.find(query, {}, {limit: 5}, function (err, doc) {
+        if (err)
+            return self.throw500(err);
+
+        return self.json(doc);
+
+    });
+}
---
+++ models/zipcode.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+		Schema = mongoose.Schema;
+
+
+/**
+ * Zip Code Schema
+ */
+
+var ZipCodeSchema = new Schema({
+	code: String,
+	insee: String,
+	city: String,
+	department: String,
+	country: String,
+	gps: [Number]
+});
+
+exports.Schema = mongoose.model('zipCode', ZipCodeSchema, 'ZipCode');
+exports.name = 'zipCode';
